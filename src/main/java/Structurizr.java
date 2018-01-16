a7f74caec04bfcff816f8aeb2464a72ff2ddecd8
hoangdo <dophuonghoang@gmail.com>
2018-01-17 06:43:32 +0700

    Y.

--- src/main/java/Structurizr.java
+++ src/main/java/Structurizr.java
@@ -2,10 +2,7 @@ import com.structurizr.Workspace;
 import com.structurizr.api.StructurizrClient;
 import com.structurizr.documentation.Format;
 import com.structurizr.documentation.StructurizrDocumentationTemplate;
-import com.structurizr.model.Model;
-import com.structurizr.model.Person;
-import com.structurizr.model.SoftwareSystem;
-import com.structurizr.model.Tags;
+import com.structurizr.model.*;
 import com.structurizr.view.*;
 
 /**
@@ -13,26 +10,43 @@ import com.structurizr.view.*;
  */
 public class Structurizr {
 
-    private static final long WORKSPACE_ID = 1234;
-    private static final String API_KEY = "key";
-    private static final String API_SECRET = "secret";
+    private static final long WORKSPACE_ID = 38067;
+    private static final String API_KEY = "e2d229b2-314c-46d5-a8ff-9bf6b646b2ad";
+    private static final String API_SECRET = "4a4162b4-82c3-4f01-b285-130c4cd0acd1";
 
     public static void main(String[] args) throws Exception {
         // a Structurizr workspace is the wrapper for a software architecture model, views and documentation
-        Workspace workspace = new Workspace("Getting Started", "This is a model of my software system.");
+        Workspace workspace = new Workspace("System context", "This is a model of Y");
         Model model = workspace.getModel();
 
         // add some elements to your software architecture model
-        Person user = model.addPerson("User", "A user of my software system.");
-        SoftwareSystem softwareSystem = model.addSoftwareSystem("Software System", "My software system.");
-        user.uses(softwareSystem, "Uses");
+        Person user = model.addPerson("Consumer", "An end consumer");
+        SoftwareSystem cooperate = model.addSoftwareSystem("Cooperate", "A cooperate consumer");
+        SoftwareSystem softwareSystem = model.addSoftwareSystem("Y", "Personal information central");
+        user.uses(softwareSystem, "Enter personal information ");
+        cooperate.uses(softwareSystem, "Retrieve or manage personal information");
 
         // define some views (the diagrams you would like to see)
         ViewSet views = workspace.getViews();
-        SystemContextView contextView = views.createSystemContextView(softwareSystem, "SystemContext", "An example of a System Context diagram.");
+        SystemContextView contextView = views.createSystemContextView(softwareSystem, "SystemContext", "Y");
         contextView.setPaperSize(PaperSize.A5_Landscape);
         contextView.addAllSoftwareSystems();
         contextView.addAllPeople();
+
+        Container webApplication = softwareSystem.addContainer("Web Application", "Personal information central",
+                "Spring Boot");
+        Container relationalDatabase = softwareSystem.addContainer("Relational Database",
+                "Stores information regarding the personal data",
+                "Relational Database Schema");
+
+        user.uses(webApplication, "Uses", "HTTPS");
+        cooperate.uses(webApplication, "Uses", "HTTPS");
+        webApplication.uses(relationalDatabase, "Reads from and writes to", "JDBC");
+
+        ContainerView containerView = views.createContainerView(softwareSystem, "Y", "Y description");
+        containerView.setPaperSize(PaperSize.A5_Landscape);
+        containerView.addAllContainers();
+        containerView.addAllPeople();
 
         // add some documentation
         StructurizrDocumentationTemplate template = new StructurizrDocumentationTemplate(workspace);
@@ -45,6 +59,7 @@ public class Structurizr {
         Styles styles = views.getConfiguration().getStyles();
         styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
         styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff").shape(Shape.Person);
+        styles.addElementStyle(Tags.CONTAINER).background("#1168bd").color("#ffffff");
 
         uploadWorkspaceToStructurizr(workspace);
     }
